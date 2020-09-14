102b33e8dcaca1d46123e454db5163474337bcab
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2020-09-15 02:52:55 +0530

    commiting changes

--- js/script.js
+++ js/script.js
@@ -2,3 +2,41 @@ $(window).on("load", function () {
   $("#status").fadeOut();
   $("#preloader").delay(350).fadeOut();
 });
+
+/* Team */
+$(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 2,
+    autoplay: true,
+    smartSpeed: 600,
+    loop: true,
+    autoplayHoverPause: true,
+    nav: true,
+    dots: false,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+  });
+});
+
+/*Progress Bars*/
+
+$(function () {
+  $("#progress-elements").waypoint(
+    function () {
+      $(".progress-bar").each(function () {
+        $(this).animate(
+          {
+            width: $(this).attr("aria-valuenow") + "%",
+          },
+          500
+        );
+      });
+      this.destroy();
+    },
+    {
+      offset: "bottom-in-view",
+    }
+  );
+});
